5f5224496c60ed511cd268f2785c6cb8523d6d05
Ben Guiden <benguiden@hotmail.co.uk>
2018-02-19 00:02:21 +0000

    Added boid parent class.

---
+++ Assets/Scripts/AI/BoidActor.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FreeSpace{
+
+    public class BoidActor : MonoBehaviour{
+
+        #region Public Variables
+        List<BoidBehaviour> behaviours;
+        #endregion
+    }
+
+}
--- Assets/Scripts/AI/Pathfollowing/PathFollower.cs
+++ Assets/Scripts/AI/Pathfollowing/PathFollower.cs
@@ -4,19 +4,24 @@ using UnityEngine;
 
 namespace FreeSpace{
 
-	public class PathFollower : MonoBehaviour {
+    public class PathFollower : MonoBehaviour{
 
-        //Change To Job
+        //Change To Job System At Some Point
 
-		#region Public Variables
-        [Header("Path")]
-		public PathDatabase pathDatabase;
-		public int pathIndex = 0;
-		#endregion
+        #region Public Variables
+        [Header ("Path")]
+        public PathDatabase pathDatabase;
+        public int pathIndex = 0;
+        #endregion
 
+        #region Movement
+        [Space]
+        [Header ("Movement")]
+        public float speed;
+        public AnimationCurve curve;
+        #endregion
 
-
-	}
+    }
     
 
 }

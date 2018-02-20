4f28bfac7146f6c7d904fa0d2f0ccf8bf348411f
Ben Guiden <benguiden@hotmail.co.uk>
2018-02-20 20:15:30 +0000

    Added pathfollowing behaviour and working boid actor.

--- Assets/Scripts/AI/BoidBehaviour.cs
+++ Assets/Scripts/AI/BoidBehaviour.cs
@@ -4,7 +4,32 @@ using UnityEngine;
 
 namespace FreeSpace{
 
+    [System.Serializable]
     public class BoidBehaviour{
+
+        #region Protected Variables
+        protected BoidActor boid;
+        #endregion
+
+        #region Constructors
+        public BoidBehaviour(BoidActor boidActor) {
+            boid = boidActor;
+        }
+        #endregion
+
+        #region Data Methods
+        public void SetBoidActor(BoidActor boidActor) {
+            boid = boidActor;
+        }
+        #endregion
+
+        #region Boid Methods
+        public virtual void Awake() { }
+        public virtual void Start() { }
+        public virtual void Update() { }
+        public virtual void OnDrawGizmos() { }
+        public virtual void OnValidate() { }
+        #endregion
 
     }
 
--- Assets/Scripts/AI/Pathfollowing/PathFollower.cs
+++ Assets/Scripts/AI/Pathfollowing/PathFollower.cs
@@ -4,23 +4,95 @@ using UnityEngine;
 
 namespace FreeSpace{
 
-    public class PathFollower : MonoBehaviour{
+    [System.Serializable]
+    public class PathFollower : BoidBehaviour{
 
         //Change To Job System At Some Point
 
         #region Public Variables
         [Header ("Path")]
         public PathDatabase pathDatabase;
-        public int pathIndex = 0;
-        #endregion
+        public uint pathIndex = 0;
+        public Vector3 pathPositionOffset;
 
-        #region Movement
-        [Space]
         [Header ("Movement")]
-        public float speed;
-        public AnimationCurve curve;
+        public uint pointIndex;
+        public float followSpeed = 5f;
+        public float pointChangeDistance = 5f;
         #endregion
 
+        #region Private Variables
+        private Path path = null;
+        public Path Path { get { return path; } }
+        #endregion
+
+        #region Constructors
+        public PathFollower(BoidActor boidActor) : base (boidActor) {
+
+        }
+        #endregion
+
+        #region Boid Methods
+        public override void Awake() {
+            RefreshPath ();
+        }
+
+        public override void Update() {
+            if (path != null) {
+                CheckNextPoint ();
+                LookAtPoint ();
+                MoveTowardsPoint ();
+            }
+        }
+
+        public override void OnDrawGizmos() {
+            if (path != null) {
+                if (pointIndex < path.points.Count) {
+                    Gizmos.color = new Color (path.color.r, path.color.g, path.color.b, path.color.a / 2f);
+                    int lastPoint = (int)pointIndex - 1;
+                    if (lastPoint < 0)
+                        lastPoint = path.points.Count - 1;
+                    Gizmos.DrawWireCube (path.points[lastPoint] + pathPositionOffset, new Vector3 (2f, 2f, 2f));
+                    Gizmos.DrawLine (path.points[lastPoint] + pathPositionOffset, path.points[(int)pointIndex] + pathPositionOffset);
+                    Gizmos.color = path.color;
+                    Gizmos.DrawWireCube (path.points[(int)pointIndex] + pathPositionOffset, new Vector3 (2f, 2f, 2f));
+                }
+            }
+        }
+        #endregion
+
+        #region Movement Methods
+        private void RefreshPath() {
+            if (pathDatabase == null) {
+                Debug.LogWarning ("Warning: Path Database reference on " + boid.gameObject.name + " set to null.\n");
+                return;
+            } else {
+                if (pathIndex >= pathDatabase.paths.Count) {
+                    Debug.LogError ("Error: Path index out of range for " + boid.gameObject.name + " game object.\n");
+                    Debug.Break ();
+                    return;
+                } else {
+                    path = pathDatabase.paths[(int)pathIndex];
+                }
+            }
+        }
+
+        private void CheckNextPoint() {
+            if (Vector3.Distance(boid.transform.position, path.points[(int)pointIndex] + pathPositionOffset) <= pointChangeDistance) {
+                pointIndex = (uint)((pointIndex + 1) % path.points.Count);
+            }
+        }
+
+        private void LookAtPoint() {
+            boid.transform.LookAt (path.points[(int)pointIndex] + pathPositionOffset);
+        }
+
+        private void MoveTowardsPoint() {
+            boid.AddForwardAcceleration (followSpeed);
+        }
+        #endregion
+
+        
     }
     
 

bdeaf1e25c0aa824dde73226acf8f3168c123f42
benguiden <benguiden@hotmail.co.uk>
2018-03-26 15:06:18 +0100

    Added Pursue behaviour.

---
+++ Assets/Scripts/AI/Behaviours/Pursue.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FreeSpace
+{
+
+    [RequireComponent (typeof (BoidActor))]
+    public class Pursue : BoidBehaviour{
+
+        #region Public Variables
+        [Header ("Pursuing")]
+        public BoidActor target;
+
+        [Header ("Movement")]
+        public float desiredDistance = 50f;
+        #endregion
+
+        #region Private Variables
+        private Vector3 desiredForward = new Vector3 ();
+        private float deltaSpeed;
+        #endregion
+
+    }
+
+}
